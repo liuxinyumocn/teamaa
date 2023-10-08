a01944d8af7058e65e7cdd09e4e222eea3dd9bd1
nebulaliu <nebulaliu@tencent.com>
2023-10-09 00:38:43 +0800

    feat: html

--- html/src/App.vue
+++ html/src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img style="width: 200px; height: 200px;" src="./assets/AApay.png">
+    <el-container class="container">
+      <el-main>
+        <div class="block">
+          <span class="demonstration">请选择本次Team总人数 n = {{ num }} 这决定了你的表格权重列数</span>
+          <el-slider v-model="num" :min="1" :max="50"></el-slider>
+          <el-button type="primary">选择本地 CSV 文件</el-button>
+          <el-button type="success">使用说明</el-button>
+        </div>
+      </el-main>
+      <el-footer>Powered By Nebula</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      num: 1,
+    }
   }
 }
 </script>
@@ -24,5 +39,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  /* background-color: #2c3e50; */
+}
+
+.container {
+  width: 100%;
+  max-width: 800px;
+  margin: 10px auto;
 }
 </style>
--- html/src/components/HelloWorld.vue
+++ html/src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
-    <ul>
+    <ul><el-button type="success">成功按钮</el-button>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
